a6bdca2a8b118705ff717558c6138ab9099b7842
Douglas Graham <douglas.r.graham.6@gmail.com>
2025-12-05 15:34:43 +0000

    Clean up mongo tests somewhat.

--- src/store/database/TestMongo.py
+++ src/store/database/TestMongo.py
@@ -5,29 +5,28 @@ import unittest
 from unittest.mock import patch
 
 class TestMongoDb(unittest.TestCase):
-    def setUp(self):
+    @patch('store.database.mongo.pymongo.MongoClient')
+    def setUp(self, mongo_mock):
         self.mock_env = {'MONGODB_HOST': 'host', 'MONGODB_DATABASE': 'db'}
-        self.mongo_client_namespace = 'store.database.mongo.pymongo.MongoClient'
         with patch.dict(os.environ, self.mock_env):
-            with patch(self.mongo_client_namespace) as mongo_mock:
-                self.db = MongoDb()
+            self.db = MongoDb()
 
     def test_upsert(self):
         test_data = [{'test': 'test'}]
-        with patch(self.mongo_client_namespace) as mongo_mock:
-            self.db.database[self.db.collection_name] = unittest.mock.Mock()
-            self.db.upsert_many(test_data)
 
-            test_item = test_data[0]
-            update = pymongo.UpdateOne(test_item, {'$set': test_item}, upsert=True)
-            self.db.database[self.db.collection_name].bulk_write.assert_called_with(
-                [update]
-            )
+        self.db.database[self.db.collection_name] = unittest.mock.Mock()
+        self.db.upsert_many(test_data)
+
+        test_item = test_data[0]
+        update = pymongo.UpdateOne(test_item, {'$set': test_item}, upsert=True)
+        self.db.database[self.db.collection_name].bulk_write.assert_called_with(
+            [update]
+        )
 
     def test_upsert_with_nothing(self):
         test_data = []
-        with patch(self.mongo_client_namespace) as mongo_mock:
-            self.db.database[self.db.collection_name] = unittest.mock.Mock()
-            self.db.upsert_many(test_data)
 
-            self.db.database[self.db.collection_name].bulk_write.assert_not_called()
+        self.db.database[self.db.collection_name] = unittest.mock.Mock()
+        self.db.upsert_many(test_data)
+
+        self.db.database[self.db.collection_name].bulk_write.assert_not_called()
