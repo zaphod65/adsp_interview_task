8aa1ce40833473dd717aadd2f780408ad5a8ac34
Douglas Graham <douglas.r.graham.6@gmail.com>
2025-12-05 14:02:39 +0000

    Remove redundant comments.

--- src/api/api.py
+++ src/api/api.py
@@ -5,10 +5,6 @@ from urllib3.exceptions import HTTPError
 
 class ApiClient:
     def request_searches(force: str, date: str) -> list[dict]:
-        # TODO: configurable date
-        # On some testing it seems that data is not available for this or last
-        # month, API returns 502
-
         try:
             response = requests.get(
                 'https://data.police.uk/api/stops-force',
