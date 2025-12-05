e85f21b96217b1f23b89905313ae09d350c56765
Douglas Graham <douglas.r.graham.6@gmail.com>
2025-12-05 14:38:54 +0000

    Add base unit test for store, allow injection of database.

---
+++ src/store/TestStore.py
@@ -0,0 +1,18 @@
+from store import Store
+import unittest
+from unittest.mock import patch
+
+class TestStore(unittest.TestCase):
+    def setUp(self):
+        self.mock_database = unittest.mock.Mock()
+        self.store = Store(self.mock_database)
+
+    def test_upsert(self):
+        test_data = [{'test': 'test'}]
+        self.store.mass_upsert(test_data)
+
+        self.mock_database.upsert_many.assert_called_with(test_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- src/store/store.py
+++ src/store/store.py
@@ -2,8 +2,11 @@ from .database.mongo import MongoDb
 
 
 class Store:
-    def __init__(self):
-        self.database = MongoDb()
+    def __init__(self, db = None):
+        if db is None:
+            self.database = MongoDb()
+        else:
+            self.database = db
 
 
     def mass_upsert(self, items: list[dict]):
